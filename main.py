272b5f663914d8fae355c2d2555da622ec565509
Your Name <you@example.com>
2019-03-12 17:47:13 +0100

    faut un message

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-from object-detector.py import *
+from object-detector import *
+
+from picamera import picamera
+from time import sleep
+
+
+camera = PiCamera()
+camera.start_preview()
+#Initialisation de la movidius
+device = open_ncs_device()
+graph = load_graph( device )
+
+
+while(True) :
+    #faire la capture d'ecran
+    camera.capture('image_pour_detection.jpg')
+
+    
+    
+
+    img_draw = skimage.io.imread( 'image_pour_detection.jpg' )
+    img = pre_process_image( img_draw )
+    infer_image( graph, img )
+
+    
+
+    #traiter l'image avec object-detector
+    #sorties
+
+
+close_ncs_device( device, graph )
