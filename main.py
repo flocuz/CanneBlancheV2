1896c7fc6ae42a5d8009d6e1f768c6594c33668e
Samuel Bamba <smail.bamba@gmail.com>
2019-03-13 16:20:09 +0100

    Ajout text-to-speech

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from object-detector import *
 
 from picamera import picamera
 from time import sleep
+# Text to speech
+import pyttsx3
 
+# Initialisation
+engine = pyttsx3.init()
 # Angle camera 62.2 x 48.8 degrees
 camera = PiCamera()
 camera.start_preview()
 #Initialisation de la movidius
 device = open_ncs_device()
 graph = load_graph( device )
-
 
 while(True) :
     #faire la capture d'ecran
@@ -26,6 +29,10 @@ while(True) :
 
     #traiter l'image avec object-detector
     #sorties
+    
+    #engine.say("I will speak this text")
+    #engine.runAndWait()
 
-
+camera.stop_preview()
+engine.stop()
 close_ncs_device( device, graph )
