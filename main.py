a9517ef6af76186c81161434bd70b36376e1b1dd
Samuel Bamba <smail.bamba@gmail.com>
2019-03-13 16:58:21 +0100

    Ajout librairie lidar

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from object-detector import *
 
+# Camera
 from picamera import picamera
 from time import sleep
 # Text to speech
 import pyttsx3
+# Hokuyo lidar
+import serial
+from hokuyo.driver import hokuyo
+from hokuyo.tools import serial_port
 
 # Initialisation
 engine = pyttsx3.init()
@@ -14,20 +19,40 @@ camera.start_preview()
 device = open_ncs_device()
 graph = load_graph( device )
 
+uart_port = '/dev/ttyACM0'
+uart_speed = 19200
+laser_serial = serial.Serial(port=uart_port, baudrate=uart_speed, timeout=0.5)
+port = serial_port.SerialPort(laser_serial)
+laser = hokuyo.Hokuyo(port)
+
 while(True) :
     #faire la capture d'ecran
     camera.capture('image_pour_detection.jpg')
 
-    
-    
-
     img_draw = skimage.io.imread( 'image_pour_detection.jpg' )
     img = pre_process_image( img_draw )
-    infer_image( graph, img )
-
-    
 
     #traiter l'image avec object-detector
+    tab = infer_image_v2( graph, img )
+
+    #donnees lidar
+    
+    print(laser.laser_on())
+    print(laser.get_single_scan())
+    print(laser.get_version_info())
+    print(laser.get_sensor_specs())
+    print(laser.get_sensor_state())
+    print(laser.set_high_sensitive())\
+    print(laser.set_high_sensitive(False))
+    print('---')
+    print(laser.set_motor_speed(10))
+    print('---')
+    print(laser.set_motor_speed())
+    print('---')
+    print(laser.reset())
+    print('---')
+    print(laser.laser_off())
+
     #sorties
     
     #engine.say("I will speak this text")
